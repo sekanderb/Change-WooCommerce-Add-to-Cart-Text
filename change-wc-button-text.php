1146afe3fa833e99dc67f5f566d960509cda5e4d
Sekander Badsha <badsha_eee@yahoo.com>
2015-07-27 22:15:28 +0600

    Create change-wc-button-text.php

---
+++ change-wc-button-text.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+Plugin Name: Change WooCommerce Add to Cart Text
+Plugin URI: http://www.nsp-code.com
+Description: Lorem Ipsome Dolor
+Author: Sekander Badsha
+Author URI: http://sekander.pro 
+Version: 0.1
+*/
+
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );
+
+function woo_custom_cart_button_text() {
+	//this text will be displayed on the single product page
+        return __( $options = get_option( 'single_button_text', false ), 'wctext' );
+ 
+}
+
+add_filter( 'woocommerce_product_add_to_cart_text' , 'custom_woocommerce_product_add_to_cart_text' );
+/**
+ * custom_woocommerce_template_loop_add_to_cart
+*/
+function custom_woocommerce_product_add_to_cart_text() {
+	global $product;
+	
+	$product_type = $product->product_type;
+	
+	switch ( $product_type ) {
+		case 'external':
+			return __( $options = get_option( 'external_button_text', false ), 'wctext' );
+		break;
+		case 'grouped':
+			return __( $options = get_option( 'grouped_button_text', false ), 'wctext' );
+		break;
+		case 'simple':
+			return __( $options = get_option( 'simple_button_text', false ), 'wctext' );
+		break;
+		case 'variable':
+			return __( $options = get_option( 'variable_button_text', false ), 'wctext' );
+		break;
+		default:
+			return __( 'Read More', 'wctext' );
+	}
+	
+}
+
+include_once 'wctext-settings.php';
+
+?>
